b85b8bbd1ea3fa197dea05e7a464e009069fe395
EliBui <91813317+EliBui@users.noreply.github.com>
2024-04-24 05:50:08 +0000

    init_student_array

--- hw5.asm
+++ hw5.asm
@@ -67,6 +67,85 @@ print_student:
 	jr $ra
 	
 init_student_array:
+	# $s0 = int num_students
+	# $s1 = int id_list[]
+	# $s2 = int credits_list[]
+	# $s3 = char *name
+	# $s4 = struct student records[]
+	# $s5 = counter
+	
+	# save $ra before calling init_student
+	addi $sp, $sp, -28 	# make space on stack for 7 registers
+	sw $ra, 24($sp)		# save $ra on stack
+	sw $s0, 20($sp)		# save $s0 on stack
+	sw $s1, 16($sp)		# save $s1 on stack
+	sw $s2, 12($sp)		# save $s2 on stack
+	sw $s3, 8($sp)		# save $s3 on stack
+	sw $s4, 4($sp)		# save $s4 on stack
+	sw $s5, 0($sp)		# save $s5 on stack
+	
+	# transfer $a to $t
+	move $s0, $a0
+	move $s1, $a1
+	move $s2, $a2
+	move $s3, $a3
+	
+	# get address of records and put it on $s4
+	move $s4, $sp
+	
+	# initialize a counter at $s5
+	li $s5, 0
+	
+	loopRecord:
+		beqz $s0, recordDone	# break if num_students == 0
+
+		# calculate offset and store in $t0
+		sll $t0, $s5, 2 	# $t0 = counter * 4
+		
+		# add offset to id and store id value in $a0
+		add $t1, $s1, $t0
+		lw $a0, 0($t1)
+		
+		# add offset to credits and store credits value in $a1
+		add $t1, $s2, $t0
+		lw $a1, 0($t1)
+		
+		# put address of the first letter of name in $a2
+		move $a2, $s3
+		
+		# calculate offset for record and store in $t0
+		sll $t0, $s5, 3 	# $t0 = counter * 8
+		
+		# put address of the record struct in $a3
+		add $t1, $s4, $t0
+		move $a3, $t1
+		
+		jal init_student	# call init_student
+		move $a0, $a3
+		jal print_student
+		
+		addi $s5, $s5, 1	# increment counter i++
+		
+		beq $s0, $s5, recordDone # break if num_students == counter
+		
+		# if not all students are initialized, then move on to next name
+		loopName:
+			lb $t1, 0($s3)
+			beqz $t1, nameDone # exit loop if null
+			addi $s3, $s3, 1 # next char
+			j loopName
+		nameDone:
+		addi $s3, $s3, 1 	# skip \0
+		j loopRecord
+	recordDone:
+	lw $s5, 0($sp)		# restore $s5 from stack
+	lw $s4, 4($sp)		# restore $s4 from stack
+	lw $s3, 8($sp)		# restore $s3 from stack
+	lw $s2, 12($sp)		# restore $s2 from stack
+	lw $s1, 16($sp)		# restore $s1 from stack
+	lw $s0, 20($sp)		# restore $s0 from stack
+	lw $ra, 24($sp) 	# restore $ra from stack
+	addi $sp, $sp, 28	# deallocate stack space
 	jr $ra
 	
 insert:
